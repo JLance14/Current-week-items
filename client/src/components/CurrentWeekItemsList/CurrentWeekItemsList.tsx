59ebafcd932e91b068b682655456fee29201a2d1
Jonathan <fbmark14@gmail.com>
2021-09-16 17:04:04 -0400

    feat(CurrentWeekItemsList): add styling

--- client/src/components/CurrentWeekItemsList/CurrentWeekItemsList.tsx
+++ client/src/components/CurrentWeekItemsList/CurrentWeekItemsList.tsx
@@ -2,8 +2,18 @@ import Typography from '@material-ui/core/Typography';
 import React, { useEffect, useState } from "react";
 import UrlExtractingService from "../../services/url-extracting-service";
 import itemFetchingService from "../../services/item-fetching-service";
+import { Card, CardContent, CardHeader, CardMedia, WithStyles, withStyles } from "@material-ui/core";
 
-function CurrentWeekItemsList() {
+const styles = () => ({
+    productTitle: {
+        fontWeight: 'bold'
+    },
+    card: {
+        margin: '2rem'
+    }
+}) as const;
+
+const CurrentWeekItemsList: React.FunctionComponent<WithStyles<typeof styles>> = ({classes}) => {
     const [items, setItems] = useState([])
 
     // @ts-ignore
@@ -15,16 +25,26 @@ function CurrentWeekItemsList() {
     return (
         <div>
         { items && items.map((currentWeekItem: any) => (
-            <div>
-                <Typography variant="h3">id: {currentWeekItem.item.id}</Typography>
-                <Typography variant="h3">name: {currentWeekItem.item.displayName}</Typography>
-                <Typography variant="h3">category: {currentWeekItem.item.category}</Typography>
-                <Typography variant="h3">status: {currentWeekItem.item.status}</Typography>
-                <img src={UrlExtractingService.extractUrlFromMetadata(currentWeekItem.item.metaData)} />
-            </div>
+            <Card className={classes.card}>
+                <CardHeader
+                    title={currentWeekItem.item.displayName}
+                    titleTypographyProps={{variant:'h3' }}
+                    classes={{
+                        title: classes.productTitle
+                    }}
+                />
+                <CardMedia component="img"
+                               image={UrlExtractingService.extractUrlFromMetadata(currentWeekItem.item.metaData)} />
+                <CardContent>
+                    <Typography variant="h4" align="left" className={classes.productTitle}><b>ID:</b> {currentWeekItem.item.id}</Typography>
+                    <Typography variant="h4" align="left"><b>name:</b> {currentWeekItem.item.displayName}</Typography>
+                    <Typography variant="h4" align="left"><b>category:</b> {currentWeekItem.item.category}</Typography>
+                    <Typography variant="h4" align="left"><b>status:</b> {currentWeekItem.item.status}</Typography>
+                </CardContent>
+            </Card>
                 ))}
         </div>
     );
 }
 
-export default CurrentWeekItemsList;
+export default withStyles(styles)(CurrentWeekItemsList);
