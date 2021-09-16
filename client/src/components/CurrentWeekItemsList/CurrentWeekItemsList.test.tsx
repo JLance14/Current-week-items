3f1d395abe658b4b097bb3211edee6750c692817
Jonathan <fbmark14@gmail.com>
2021-09-16 19:31:23 -0400

    feat(CurrentWeekItemList unit tests): implement unit tests for component

--- client/src/components/CurrentWeekItemsList/CurrentWeekItemsList.test.tsx
+++ client/src/components/CurrentWeekItemsList/CurrentWeekItemsList.test.tsx
@@ -1,15 +1,74 @@
-import { shallow } from 'enzyme';
-import { findByDataTestId } from '../../utils/testUtils';
-import CurrentWeekItemsList from './CurrentWeekItemsList';
+import { CurrentWeekItemsList } from './CurrentWeekItemsList';
+import {shallow, ShallowWrapper} from "enzyme";
+import itemFetchingService from "../../services/item-fetching-service";
+import {findByDataTestId} from "../../utils/testUtils";
+import React from "react";
+
+jest.mock("../../services/item-fetching-service")
+
+const emptyItems = [] as any;
+
+const dummyItems = [
+    {
+        id: "111",
+        deliveryWeek: "2021-09-12",
+        metadata: "",
+        item: {
+            displayName: "",
+            metaData: "",
+            category: "",
+            status: ""
+        }
+    },
+    {
+        id: "112",
+        deliveryWeek: "2021-09-12",
+        metadata: "",
+        item: {
+            displayName: "",
+            metaData: "",
+            category: "",
+            status: ""
+        }
+    },
+    {
+        id: "113",
+        deliveryWeek: "2021-09-12",
+        metadata: "",
+        item: {
+            displayName: "",
+            metaData: "",
+            category: "",
+            status: ""
+        }
+    }
+]
 
 describe('CurrentWeekItemsList', () => {
-    describe('menu title', () => {
-        it('should display list title', () => {
-            const wrapper = shallow(<CurrentWeekItemsList />);
-            const listTitle = findByDataTestId(wrapper, 'list-title')
-            expect(listTitle.text()).toBe('Current week items');
+    let wrapper = {} as any;
+    describe('given a list of items', () => {
+        beforeEach(() => {
+            (itemFetchingService.fetchFromCurrentWeek as jest.Mock).mockReturnValue(dummyItems)
+            wrapper = createComponent();
+        })
+        it('should display items', () => {
+            const listTitle = findByDataTestId(wrapper, 'itemCard')
+            expect(listTitle).not.toHaveLength(0);
+        });
+    });
+
+    describe('given an empty list of items', () => {
+        beforeEach(() => {
+            (itemFetchingService.fetchFromCurrentWeek as jest.Mock).mockReturnValue(emptyItems)
+            wrapper = createComponent();
+        })
+        it('should not display any item', () => {
+            const listTitle = findByDataTestId(wrapper, 'itemCard')
+            expect(listTitle).toHaveLength(0);
         });
     });
 })
+
+const createComponent = (): ShallowWrapper => shallow(<CurrentWeekItemsList  classes={{} as any}/> )
 
 export {};
--- client/src/components/CurrentWeekItemsList/CurrentWeekItemsList.tsx
+++ client/src/components/CurrentWeekItemsList/CurrentWeekItemsList.tsx
@@ -13,38 +13,55 @@ const styles = () => ({
     }
 }) as const;
 
-const CurrentWeekItemsList: React.FunctionComponent<WithStyles<typeof styles>> = ({classes}) => {
-    const [items, setItems] = useState([])
+interface Props extends WithStyles<typeof styles> {}
 
-    // @ts-ignore
-    useEffect(async () => {
+interface State{
+    items: [];
+}
+
+export class CurrentWeekItemsList extends React.Component<Props, State> {
+    constructor(props: Readonly<Props>) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+    }
+
+    async componentDidMount() {
         const result = await itemFetchingService.fetchFromCurrentWeek() as any;
-        setItems(result)
-    }, [])
+        this.setState({ items: result });
+    }
 
-    return (
-        <div>
-        { items && items.map((currentWeekItem: any) => (
-            <Card className={classes.card}>
-                <CardHeader
-                    title={currentWeekItem.item.displayName}
-                    titleTypographyProps={{variant:'h3' }}
-                    classes={{
-                        title: classes.productTitle
-                    }}
-                />
-                <CardMedia component="img"
-                               image={UrlExtractingService.extractUrlFromMetadata(currentWeekItem.item.metaData)} />
-                <CardContent>
-                    <Typography variant="h4" align="left" className={classes.productTitle}><b>ID:</b> {currentWeekItem.item.id}</Typography>
-                    <Typography variant="h4" align="left"><b>name:</b> {currentWeekItem.item.displayName}</Typography>
-                    <Typography variant="h4" align="left"><b>category:</b> {currentWeekItem.item.category}</Typography>
-                    <Typography variant="h4" align="left"><b>status:</b> {currentWeekItem.item.status}</Typography>
-                </CardContent>
-            </Card>
+    render() {
+        const { classes } = this.props;
+        const { items } = this.state;
+
+        return (
+            <div>
+                { items && items.map((currentWeekItem: any) => (
+                    <Card className={classes.card} data-testid="itemCard" key={currentWeekItem.id}>
+                        <CardHeader
+                            title={currentWeekItem.item.displayName}
+                            titleTypographyProps={{variant:'h3' }}
+                            classes={{
+                                title: classes.productTitle
+                            }}
+                        />
+                        <CardMedia component="img"
+                                   image={UrlExtractingService.extractUrlFromMetadata(currentWeekItem.item.metaData)} />
+                        <CardContent>
+                            <Typography variant="h4" align="left"><b>ID:</b> {currentWeekItem.item.id}</Typography>
+                            <Typography variant="h4" align="left"><b>name:</b> {currentWeekItem.item.displayName}</Typography>
+                            <Typography variant="h4" align="left"><b>category:</b> {currentWeekItem.item.category}</Typography>
+                            <Typography variant="h4" align="left"><b>status:</b> {currentWeekItem.item.status}</Typography>
+                        </CardContent>
+                    </Card>
                 ))}
-        </div>
-    );
+            </div>
+        )
+    }
 }
 
 export default withStyles(styles)(CurrentWeekItemsList);
+
